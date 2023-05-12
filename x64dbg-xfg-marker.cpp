db26c27ae0f42ad831b1a848c5b4b9d5c51614c7
Michael Maltsev <4129781+m417z@users.noreply.github.com>
2023-05-12 15:22:24 +0300

    v1.1.1

--- x64dbg-xfg-marker.cpp
+++ x64dbg-xfg-marker.cpp
@@ -3,8 +3,8 @@
 #include "resource.h"
 
 #define PLUGIN_NAME "XFG Marker"
-#define PLUGIN_VERSION 11
-#define PLUGIN_VERSION_STR "1.1"
+#define PLUGIN_VERSION 111
+#define PLUGIN_VERSION_STR "1.1.1"
 
 #ifndef DLL_EXPORT
 #define DLL_EXPORT __declspec(dllexport)
